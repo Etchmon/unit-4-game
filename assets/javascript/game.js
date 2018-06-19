aa1e157e63e30af573cb2c66e07cfc49458210d1
Etch <Etch.rush@gmail.com>
2018-06-18 21:04:24 -0800

    still not working somehow

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,28 @@
-$(".header").html("this dick");
+var score = 0;
+var wins = 0;
+var losses = 0;
+
+var randomNumber = Math.floor((Math.random() * 500) + 1);
+var ruby = Math.floor((Math.random() * 50) + 1);
+var emerald = Math.floor((Math.random() * 100) + 1);
+var topz = Math.floor((Math.random() * 30) + 1);
+var sapphire = Math.floor((Math.random() * 40) + 1);
+
+console.log(randomNumber);
+console.log(sapphire);
+
+function setGame(){
+    var randomNumber = Math.floor((Math.random() * 500) + 1);
+    var ruby = Math.floor((Math.random() * 50) + 1);
+    var emerald = Math.floor((Math.random() * 100) + 1);
+    var topz = Math.floor((Math.random() * 30) + 1);
+    var sapphire = Math.floor((Math.random() * 40) + 1);
+
+    $(".thistag").html(randomNumber);
+}
+
+setGame();
+
+$("#ruby").on("click", function(){
+    $(".readme").html("hahahah")
+});
