81ce535365439cd6742b5c1c5bcbcabe87c9f99a
Etch <Etch.rush@gmail.com>
2018-06-18 22:01:26 -0800

    fixed it all wow

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,27 +2,92 @@ var score = 0;
 var wins = 0;
 var losses = 0;
 
-var randomNumber = Math.floor((Math.random() * 500) + 1);
+var randomNumber = Math.floor((Math.random() * 500) + 100);
 var ruby = Math.floor((Math.random() * 50) + 1);
-var emerald = Math.floor((Math.random() * 100) + 1);
-var topz = Math.floor((Math.random() * 30) + 1);
+var emerald = Math.floor((Math.random() * 20) + 1);
+var topaz = Math.floor((Math.random() * 30) + 1);
 var sapphire = Math.floor((Math.random() * 40) + 1);
 
-console.log(randomNumber);
+$(".thistag").html(randomNumber);
+
 console.log(sapphire);
 
 function setGame(){
-    var randomNumber = Math.floor((Math.random() * 500) + 1);
-    var ruby = Math.floor((Math.random() * 50) + 1);
-    var emerald = Math.floor((Math.random() * 100) + 1);
-    var topz = Math.floor((Math.random() * 30) + 1);
-    var sapphire = Math.floor((Math.random() * 40) + 1);
+    ruby = Math.floor((Math.random() * 50) + 1);
+    emerald = Math.floor((Math.random() * 20) + 1);
+    topaz = Math.floor((Math.random() * 30) + 1);
+    sapphire = Math.floor((Math.random() * 40) + 1);
+    randomNumber = Math.floor((Math.random() * 500) + 100);
 
     $(".thistag").html(randomNumber);
+
+    score = 0;
+
+    $(".number").html("score here")
+
+    console.log(randomNumber)
+
 }
 
-setGame();
+function win(){
+    alert("CONGRATZ M8 YOU WON!");
+    wins++;
+    $(".wins").html("Wins: " + wins);
+    setGame();
+}
+
+function lose(){
+    alert("You lost dude");
+    losses++;
+    $(".losses").html("Losses: " + losses);
+    setGame();
+}
+
 
 $("#ruby").on("click", function(){
-    $(".readme").html("hahahah")
+    score = score + ruby;
+    $(".number").html(score);
+    if(score === randomNumber){
+            win();
+        }
+    else if ( score > randomNumber){
+            lose();
+        }
+
+});
+
+$("#sapphire").on("click", function(){
+    score = score + sapphire;
+    $(".number").html(score);
+        if(score === randomNumber){
+            win();
+        }
+        else if ( score > randomNumber){
+            lose();
+        }
+
+});
+
+$("#emerald").on("click", function(){
+    score = score + emerald;
+    $(".number").html(score);
+        if(score === randomNumber){
+            win();
+        }
+        else if ( score > randomNumber){
+            lose();
+        }
+
+});
+
+$("#topaz").on("click", function(){
+    score = score + topaz;
+    $(".number").html(score);
+        if(score === randomNumber){
+            win();
+        }
+        else if ( score > randomNumber){
+            lose();
+        }
+
 });
